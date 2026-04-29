ec567eb6fbbde81ba9d554db853a6e0c1456e630
iilovepdf <sahookumaramiya9@gmail.com>
2026-04-29 10:41:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -419,8 +419,8 @@ export default function App() {
               <div className="relative border-l-2 border-wedding-gold/20 ml-4 md:ml-0 md:pl-0 space-y-12">
                 {[
                   { title: "Ring Ceremony", time: "April 29, 2026 | 12:30 PM", loc: "Maa Ramachandi Temple, Bhobara", icon: "💍", tag: "Engagement" },
-                  { title: "Sangeet Night", time: "Coming Soon", loc: " Badapari, Tangi, Odisha", icon: "🎵", tag: "Music & Dance" },
-                  { title: "The Wedding", time: "Coming Soon", loc: " Badapari, Tangi, Odisha", icon: "🕊️", tag: "The Holy Union" }
+                  { title: "Sangeet Night", time: "Coming Soon..", loc: " Badapari, Tangi, Odisha", icon: "🎵", tag: "Music & Dance" },
+                  { title: "The Wedding", time: "Coming Soon..", loc: " Badapari, Tangi, Odisha", icon: "🕊️", tag: "The Holy Union" }
                 ].map((event, i) => (
                    <motion.div 
                      key={i}
